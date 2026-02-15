b6dc7db91e54aea4f915a96990c05ae463db9e68
Yavuz Selim Kahyaoğlu <140605846+yvzskhy@users.noreply.github.com>
2026-02-16 01:36:01 +0300

    Get photo commit
    All photos can be listed
    Specific photo with id can be listed

---
+++ src/main/java/com/yaseka/photoz/Photo.java
@@ -0,0 +1,31 @@
+package com.yaseka.photoz;
+
+public class Photo {
+
+    private String id;
+
+    private String fileName;
+
+    public Photo(String id, String fileName){
+        this.id = id;
+        this.fileName = fileName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+
+}
--- src/main/java/com/yaseka/photoz/PhotozController.java
+++ src/main/java/com/yaseka/photoz/PhotozController.java
@@ -1,12 +1,42 @@
 package com.yaseka.photoz;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.annotation.ResponseStatusExceptionResolver;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController                 //Helping spring boot to understand our controller function
 public class PhotozController {
+
+    private Map<String,Photo> db = new HashMap<>(){{
+        put("1",new Photo("1","photo1"));
+        put("2",new Photo("2","photo2"));
+    }};
+
     @GetMapping("/")            //Specify the path which function will be activated
     public String greetings(){
         return "Hello World";
+    }
+
+    @GetMapping("/photoz")
+    public Collection<Photo> getPhotos(){
+        return db.values();
+    }
+
+    @GetMapping("/photoz/{id}")
+    public Photo getPhotoWithID(@PathVariable String id) throws Throwable {
+        Photo photo = db.get(id);
+        if (photo != null){
+            return photo;
+        }
+        else {
+            throw new Throwable(String.valueOf(HttpStatus.NOT_FOUND));
+        }
     }
 }
