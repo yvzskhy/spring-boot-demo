3616fda5ce4e4c430dccd9bb0c101c81d0055395
Yavuz Selim Kahyaoğlu <140605846+yvzskhy@users.noreply.github.com>
2026-02-16 00:44:34 +0300

    Initial commit

---
+++ src/main/java/com/yaseka/photoz/PhotozApplication.java
@@ -0,0 +1,13 @@
+package com.yaseka.photoz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotozApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotozApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yaseka/photoz/PhotozApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaseka.photoz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotozApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
